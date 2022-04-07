23832932ea7882500e67f00adec8c51374e65885
Thea Kjeldsmark <tkjeldsmark@gmail.com>
2022-04-07 23:22:22 +0200

    Get rid of console error when click outside nodes

--- src/client/js/graphLoader.js
+++ src/client/js/graphLoader.js
@@ -19,60 +19,58 @@ const getGraph = () => {
 const createGraph = (graphData) => {
 
     // Create a network
-    var container = document.getElementById('graph');
+    const container = document.getElementById('graph');
 
     // Provide the data in the vis format
-    var data = {
+    const data = {
         nodes: graphData['nodes'],
         edges: graphData['edges']
     };
-    var options = {
+    const options = {
         nodes: {
             shape: "circle",
             scaling: {
-              min: 10,
-              max: 30,
+                min: 10,
+                max: 30,
             },
             font: {
-              size: 16,
-              face: "Helvetica",
+                size: 16,
+                face: "Helvetica",
             },
             color: {
                 border: '#000000',
                 background: '#ede9da',
-                border:  '#87A980',
+                border: '#87A980',
                 highlight: {
                     border: '#378805',
                     background: '#87A980'
                 },
             },
-          },
-          edges: {
+        },
+        edges: {
             width: 0.15,
             length: 240,
-          },
+        },
     };
 
-    
+
     // Initialize the network
-    var network = new vis.Network(container, data, options);
-    
+    const network = new vis.Network(container, data, options);
+
     network.on("click", function (params) {
         params.event = "[original event]";
 
         // Show the sidebar when a node is clicked
-        var sidebar = document.getElementById("sidebar");
+        const sidebar = document.getElementById("sidebar");
         sidebar.style.width = "250px";
 
         // Update the sidebar with information about the clicked node 
         const paper = graphData['nodes'].find(element => element['id'] == this.getNodeAt(params.pointer.DOM));
-        var sidebar = document.getElementById("paper-title").innerText = paper['label'];
-        var sidebar = document.getElementById("paper-id").innerText = "ID " + paper['id'];
-        
-        console.log(
-            "click event, getNodeAt returns node with ID: " + this.getNodeAt(params.pointer.DOM)
-        );
+        if (paper != null) {
+            document.getElementById("paper-title").innerText = paper['label'];
+            document.getElementById("paper-id").innerText = "ID " + paper['id'];
+        }
     });
 }
 
-export { getGraph }
+export {getGraph}
